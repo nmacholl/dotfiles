5f7da14545bda4385b6855af9debbfa1f79af1a4
Nick Macholl <nickmacholl@gmail.com>
2023-02-01 15:49:08 -0800

    Copy pasta

--- nvim/lua/topnerd/remap.lua
+++ nvim/lua/topnerd/remap.lua
@@ -8,3 +8,9 @@ vim.keymap.set("n", "Q", "<nop>")
 -- Window Management
 vim.keymap.set("n", "<C-W>z", ":wincmd _<CR>")
 vim.keymap.set("n", "<C-W>x", ":wincmd =<CR>")
+
+-- Copy Pasta
+vim.keymap.set("v", "<C-y>", [["+yi]])
+vim.keymap.set("v", "<C-x>", [["+c]])
+vim.keymap.set("v", "<C-p>", [[c<ESC>"+p]])
+vim.keymap.set("n", "<C-p>", [[<ESC>"+pa]])
