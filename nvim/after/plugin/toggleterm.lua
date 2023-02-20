26752e946d581f8d355ebfae2237cb7fd9dd2794
Nick Macholl <nickmacholl@gmail.com>
2023-02-19 19:57:20 -0800

    Updated toggleterm to remove floating window.

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,22 +1,9 @@
 -- Setup
 require("toggleterm").setup{
     autochdir = true,
+    direction = "vertical",
 }
-local Terminal = require("toggleterm.terminal").Terminal
-
--- Big Terminal
-local big_terminal = Terminal:new(
-    {
-        direction = "float",
-        float_opts = {
-            border = "double",
-        }
-    }
-)
-function _big_toggle()
-    big_terminal:toggle()
-end
 
 -- Shortcuts
-vim.keymap.set("n", "<leader>t", "<cmd>lua _big_toggle()<CR>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:ToggleTermToggleAll<CR>")
+vim.keymap.set("n", "<leader>t", ":ToggleTerm size=80<CR>")
+vim.keymap.set("t", "<leader><Esc>", "<C-\\><C-n>:ToggleTerm<CR>")
