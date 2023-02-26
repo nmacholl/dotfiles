508dcb03d95d41d0cefe9929dc9e3706a90440e0
Nick Macholl <nickmacholl@gmail.com>
2023-02-26 12:31:55 -0800

    Improved bufferline naviation shortcuts.

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -2,8 +2,10 @@
 require("bufferline").setup{
     options = {
         show_buffer_icons = false,
+        show_buffer_close_icons = false,
         diagnostics = "coc",
         enforce_regular_tabs = true,
+        sort_by = "relative_directory",
         hover = {
             enabled = true,
             delay = 200,
@@ -13,13 +15,22 @@ require("bufferline").setup{
             items = {
                 require("bufferline.groups").builtin.pinned:with({ icon = "⋄" })
             }
-        }
+        },
     },
 }
 
 -- Shortcuts
-vim.keymap.set("n", "<leader>e", ":BufferLinePick<CR>")
 vim.keymap.set("n", "<leader>p", ":BufferLineTogglePin<CR>")
+
+vim.keymap.set("n", "<leader>wh", ":BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<leader>wl", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<leader>wk", ":BufferLineGoToBuffer -1<CR>")
+vim.keymap.set("n", "<leader>wj", ":BufferLineGoToBuffer 1<CR>")
+vim.keymap.set("n", "<leader>w", ":BufferLinePick<CR>")
+
+vim.keymap.set("n", "<leader>qh", ":BufferLineCloseLeft<CR>")
+vim.keymap.set("n", "<leader>ql", ":BufferLineCloseRight<CR>")
+vim.keymap.set("n", "<leader>qk", ":BufferLineGoToBuffer -1<CR>:BufferLineCloseLeft<CR>")
+vim.keymap.set("n", "<leader>qj", ":BufferLineGoToBuffer 1<CR>:BufferLineCloseRight<CR>")
+vim.keymap.set("n", "<leader>ql", ":BufferLineCloseRight<CR>")
 vim.keymap.set("n", "<leader>q", ":BufferLinePickClose<CR>")
-vim.keymap.set("n", "<leader>q[", ":BufferLineCloseLeft<CR>")
-vim.keymap.set("n", "<leader>q]", ":BufferLineCloseRight<CR>")
