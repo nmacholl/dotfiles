1928ee370b24837fad39e4789788d40967798876
Nick Macholl <nickmacholl@gmail.com>
2023-02-26 11:55:29 -0800

    Added open automation for nvim tree

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -34,3 +34,22 @@ require("nvim-tree").setup(
 vim.keymap.set("n", "<C-n>", ":NvimTreeFocus<CR>")
 vim.keymap.set("n", "<C-t>", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<C-f>", ":NvimTreeFindFile<CR>")
+
+-- Automations
+local function open_nvim_tree(data)
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+    local real_file = vim.fn.filereadable(data.file) == 1
+    local real_directory = vim.fn.isdirectory(data.file) == 1
+
+    if not no_name and (real_file or real_directory) then
+        require("nvim-tree.api").tree.toggle({
+            file_file = true,
+            update_root = true,
+            focus = false,
+        })
+    else
+        require("nvim-tree.api").tree.open()
+    end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
