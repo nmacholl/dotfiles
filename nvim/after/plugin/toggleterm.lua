d45e4997e1e7f57d9dc94d7a0bd99a8041cc0169
Nick Macholl <nickmacholl@gmail.com>
2023-02-26 11:35:40 -0800

    More window-like toggle term.

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -2,8 +2,9 @@
 require("toggleterm").setup{
     autochdir = true,
     direction = "vertical",
+    start_in_insert = false,
 }
 
 -- Shortcuts
-vim.keymap.set("n", "<leader>t", ":ToggleTerm size=80<CR>")
-vim.keymap.set("t", "<leader><Esc>", "<C-\\><C-n>:ToggleTerm<CR>")
+vim.keymap.set("n", "<leader>t", ":ToggleTerm size=100<CR>")
+vim.keymap.set("t", "<leader><Esc>", "<C-\\><C-n><CR>")
